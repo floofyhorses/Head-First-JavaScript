e45fdd93870b3a3dcfb3d55f5b33466efac832b1
growthosedendrites <69527252+growthosedendrites@users.noreply.github.com>
2021-12-11 09:37:33 -0800

    Add files via upload
    An exercise where you guess blurred images and click to unblur and see if you were correct.

---
+++ blurgame/script.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+			var images = document.getElementsByTagName("img");
+			for ( var i = 0; i < images.length; i++) {
+				images[i].onclick = showAnswer;
+			}
+		};
+
+		function showAnswer(eventObj) {
+			var image = eventObj.target;
+			var name = image.id;
+			name = name + ".jpg";
+			image.src = name;
+
+			setTimeout(reblur, 3000, image);
+         }
+
+//LEARNING MOMENT: The photos for the unblurred image broke every time no mattered what I did
+//I finally realized that I had added an extra space in the "" when concatenating, (at line 11),which
+//made the file name not add the .jpg properly and thus not return the img file. *facepalms*
+
+
+		function reblur(image) {
+			var name = image.id;
+			name = name + "blur.jpg";
+			image.src = name;
+		}
